8567d97408546823528df251a0e61d5325a52537
Ethan <ethanpace212@gmail.com>
2025-06-11 02:20:18 +0000

    fixed get button

--- resources/views/components/get-button.blade.php
+++ resources/views/components/get-button.blade.php
@@ -1,3 +1,4 @@
-<a :href="$action" class="py-2 px-8 border-2 border-indigo-500 rounded-lg bg-indigo-200 hover:bg-indigo-100 trasition-colors duration-300 text-indigo-700">
+@props(['action', 'padding' => 8])
+<a href="{{ $action }}" class="py-2 px-{{ $padding }} border-2 border-indigo-500 rounded-lg bg-indigo-200 hover:bg-indigo-100 trasition-colors duration-300 text-indigo-700">
     {{ $slot }}
 </a>
